8db80796195b5fb2222571539d48fd733a0da990
JS <wow220713@gmail.com>
2024-05-08 17:31:46 +0900

    Backup

---
+++ src/main/java/org/fullstack4/controller/SampleController.java
@@ -0,0 +1,25 @@
+package org.fullstack4.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Log4j2
+@Controller
+public class SampleController {
+
+    @GetMapping("hello")
+    public void hello(Model model){
+        model.addAttribute("msg","Hello");
+
+    }
+    @GetMapping("/ex/ex1")
+    public void ex1(Model model){
+        List<String> list = Arrays.asList("AAA","BBB","CCC");
+        model.addAttribute("list",list);
+    }
+}
---
+++ src/main/java/org/fullstack4/controller/SampleJsonController.java
@@ -0,0 +1,18 @@
+package org.fullstack4.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Log4j2
+@RestController
+public class SampleJsonController {
+
+    @GetMapping("/arrhello")
+    public String[] arrhello(){
+        log.info("=====================");
+        return new String[]{"AAA", "BBB", "CCC"};
+
+    }
+}
